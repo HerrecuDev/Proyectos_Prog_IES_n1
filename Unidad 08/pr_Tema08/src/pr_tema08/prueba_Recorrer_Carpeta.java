e6c11454cee4e67bcb85a67524f58271563235b9
pablo <pablo@DESKTOP-TDL8D9F>
2025-03-30 22:47:17 +0200

    prueba desde Netbeans

---
+++ Unidad 08/pr_Tema08/src/pr_tema08/prueba_Recorrer_Carpeta.java
@@ -0,0 +1,40 @@
+
+
+package pr_tema08;
+
+import java.io.File;
+
+
+public class prueba_Recorrer_Carpeta {
+
+    
+    public static void main(String[] args) {
+        
+        File desktop = new File("C:\\Users\\alumnociclo\\Desktop");
+        
+        File [] archivosDirectorios = desktop.listFiles();
+        
+        
+        String tipo ="";
+        
+        for(File ficheroActual : archivosDirectorios){
+            
+            if (ficheroActual.isDirectory()) {
+                
+                tipo = "[D]";
+                
+                
+            }
+            else if(ficheroActual.isFile()){
+                
+                tipo = "[A]";
+            }
+            
+            System.out.println(tipo + " " + ficheroActual.getName());
+        }
+        
+        
+        
+    }
+
+}
