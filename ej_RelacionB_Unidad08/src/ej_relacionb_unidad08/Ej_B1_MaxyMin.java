5689840660c764f9a93b77ab19aab51ccd9a638b
alumnociclo <alumnociclo@PC111515.dptoinformatica.iesfuengirola1.net>
2025-04-01 18:30:07 +0200

    relacion B

--- ej_RelacionB_Unidad08/src/ej_relacionb_unidad08/Ej_B1_MaxyMin.java
+++ ej_RelacionB_Unidad08/src/ej_relacionb_unidad08/Ej_B1_MaxyMin.java
@@ -2,17 +2,61 @@
 
 package ej_relacionb_unidad08;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 
 public class Ej_B1_MaxyMin {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(new FileReader("C:\\Users\\alumnociclo\\Desktop\\Documentos\\Documentos\\numeros.txt"));
         
-        String ruta = "C:\\Users\\alumnociclo\\Desktop\\Documentos\\Documentos\\numeros";
+        int minimo = 0;
+        int maximo = 0;
+        boolean primerNumero = true;
+    
+        while (sc.hasNext()) {
+            
+            int number = sc.nextInt();
+            
+            if (primerNumero == true) {
+                
+                
+                minimo = number;
+                maximo = number;
+                
+                primerNumero = false;
+                
+            }
+            else{
+                
+                if (number > maximo) {
+                    
+                    maximo = number;
+                    
+                }
+                
+                if (number < minimo) {
+                    
+                    minimo = number;
+                    
+                }
+                
+            }
+            
+           
+           
+            
+        }
+        
+        System.out.println("El numero Maximo es  = " + maximo);
+        System.out.println("El numero Minimo es  = " + minimo);
+        
+        
+        
         
         
     }
---
+++ ej_RelacionB_Unidad08/src/ej_relacionb_unidad08/Ej_B2_NotasAlumnos.java
@@ -0,0 +1,47 @@
+
+
+package ej_relacionb_unidad08;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.System.out;
+import java.util.Scanner;
+
+
+public class Ej_B2_NotasAlumnos {
+
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        Scanner sc = new Scanner(new File("C:\\Users\\alumnociclo\\Desktop\\Documentos\\Documentos\\numeros.txt"));
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s;
+        
+        String frase = "";
+        
+        while ((s = br.readLine()) == null) {     
+            
+            
+         String tokens [] = s.split(" ");
+         
+         
+          
+          
+           
+            
+           
+            
+            
+        }
+        
+        
+        
+        
+    }
+
+}
