6f5abe36a92fd7d4d7908dc877745676aa80011d
HerrecuDev <pablojhc15@gmail.com>
2025-04-03 20:29:01 +0200

    fin

---
+++ Unidad 08/pr_Tema08/src/pr_tema08/Coche.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pr_tema08;
+
+/**
+ *
+ * @author alumnociclo
+ */
+public class Coche {
+    
+    private static final long serialVersionU1D = 0;
+    
+    private String matricula;
+    private int deposito;
+    
+    public Coche(String matricula , int deposito){
+        
+        this.matricula = matricula;
+        this.deposito = deposito;
+        
+        
+    }
+    
+}
---
+++ Unidad 08/pr_Tema08/src/pr_tema08/mainSerializacion.java
@@ -0,0 +1,23 @@
+
+
+package pr_tema08;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+
+public class mainSerializacion {
+
+    
+    public static void main(String[] args) {
+        
+        
+        Coche coche1 = new Coche("M", 0);
+        
+        FileOutputStream ficheroS = new FileOutputStream("");
+        ObjectOutputStream;
+        
+        
+    }
+
+}
