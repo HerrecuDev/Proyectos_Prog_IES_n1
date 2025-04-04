8c8c5f55dfd3597bd539580172de90645bcf2132
HerrecuDev <pablojhc15@gmail.com>
2025-04-04 20:01:28 +0200

    OK

--- Unidad 08/pr_Tema08/src/pr_tema08/mainSerializacion.java
+++ Unidad 08/pr_Tema08/src/pr_tema08/mainSerializacion.java
@@ -2,6 +2,7 @@
 
 package pr_tema08;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
@@ -9,13 +10,13 @@ import java.io.ObjectOutputStream;
 public class mainSerializacion {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
         
         Coche coche1 = new Coche("M", 0);
         
         FileOutputStream ficheroS = new FileOutputStream("");
-        ObjectOutputStream;
+        //ObjectOutputStream;
         
         
     }
