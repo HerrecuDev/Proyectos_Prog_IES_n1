e6b1841d032dc5a59aa0964dad60fd4a860e9229
HerrecuDev <pablojhc15@gmail.com>
2025-04-05 15:25:08 +0200

    game over

--- Unidad 07/PROG07_Tarea_2023_24_Alumnado/src/tarea07/Ejercicio03.java
+++ Unidad 07/PROG07_Tarea_2023_24_Alumnado/src/tarea07/Ejercicio03.java
@@ -1,5 +1,13 @@
 package tarea07;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 /** Ejercicio 3. Calendario de especies de plantas
  * @author Profesor
  */
@@ -15,7 +23,66 @@ public class Ejercicio03 {
         //----------------------------------------------
         
         
-        //Creo el mapa para añadir todas las especies de planta con la fecha
+        int numero_Plantas = 0;
+        int numero_Dias = 0;
+        
+        String planta_Aleatorio;
+        
+       //Primero genero un List que contenga 7 tipos de plantas :
+       
+       List<String> plantas = new ArrayList<>();
+       
+       //Añado las 7 plantas diferentes al List(Teniendo en cuenta que si se repite alguna vuelva a crear otra :
+       
+        while (numero_Plantas < 7) { 
+            
+            planta_Aleatorio = Utilidades.especiePlantaAleatoria();
+            
+            if (plantas.contains(planta_Aleatorio)) {
+                
+                planta_Aleatorio = Utilidades.especiePlantaAleatoria();
+            }
+            else{
+                 plantas.add(planta_Aleatorio);
+            
+            numero_Plantas++;
+            }
+            
+           
+            
+            
+        }
+        
+        System.out.println(plantas);
+        
+        
+        //Genero un mapa que contenga 7 dias de la semana y las plantas :
+        
+        LinkedHashMap< LocalDate , String> calendario = new LinkedHashMap<>();
+        
+        
+        
+        //Genero la fecha actual con el formato que quiero :
+        LocalDate fechaActual  = LocalDate.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
+        
+        
+        for (int i = 0; i < 7; i++) {
+            
+            formato.format(fechaActual);
+            
+            //calendario.put(fechaActual, plantas);
+            
+            
+            
+            //Actualizamos el dia :
+            fechaActual = fechaActual.plusDays(1);
+      
+            
+        }
+        
+        
         
         // No se piden datos al usuario, ya que se usa un número fijo de elementos aleatorios
         
@@ -28,6 +95,17 @@ public class Ejercicio03 {
         //           Salida de resultados
         //----------------------------------------------
         
+        
+        //IMPRIMIMOS EL CALENDARIO :
+        
+        System.out.println("El calendario es :");
+        
+        for (int i = 0; i < plantas.size(); i++) {
+            
+            System.out.println(calendario.get(i) + ":" + plantas.get(i));
+            
+            
+        }
  
     }
 }
