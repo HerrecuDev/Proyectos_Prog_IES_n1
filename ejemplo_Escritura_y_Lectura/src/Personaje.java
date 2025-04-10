b636f31999ff54e584078a77e1c81e03ba9ec09a
HerrecuDev <pablojhc15@gmail.com>
2025-04-10 20:23:56 +0200

    Ejercicio guardar Partida

--- ejemplo_Escritura_y_Lectura/src/Personaje.java
+++ ejemplo_Escritura_y_Lectura/src/Personaje.java
@@ -1,3 +1,6 @@
+
+import java.io.Serializable;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,50 @@
  *
  * @author alumnociclo
  */
-public class Personaje {
+public class Personaje implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private final int id;
+    private final String nombre;
+    
+    public Personaje(int id , String nombre){
+        
+        this.id = id;
+        this.nombre = nombre;
+        
+        
+        
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
+    
+    
+    
+    //Generamos el toString : 
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Personaje{");
+        sb.append("id=").append(id);
+        sb.append(", nombre=").append(nombre);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
     
 }
