165b7bd55c5f145edf7f61fb803f91129a305f86
HerrecuDev <pablojhc15@gmail.com>
2025-04-28 00:36:32 +0200

    modificacion ejercicio extra

---
+++ proyecto_Ej_Extra_Unidad8/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,55 @@
+package ejercicio1;
+
+/**
+ * Ejercicio 1: Lectura/escritura de una biblioteca de libros en ficheros de texto.
+ *
+ * @author apellidos y nombre del alumno
+ */
+public class Ejercicio1 {
+
+    /**
+     * Método principal.
+     *
+     * @param args argumentos que recibe el método
+     */
+    public static void main(String args[]) {
+
+        //----------------------------------------------
+        //          Declaracion de variables 
+        //----------------------------------------------
+        // Constantes
+        // Variables de entrada
+        // Variables de salida
+        // Variables auxiliares
+
+        //----------------------------------------------
+        //       Entrada de datos + Procesamiento
+        //----------------------------------------------
+        // Abrimos archivo de contactos ListadoLibros.txt
+        System.out.println("Abriendo archivo de libros...");
+
+        
+        
+
+        System.out.println("Cerrando archivo de libros...");
+
+        System.out.println();
+
+        //----------------------------------------------
+        //              Salida de resultados 
+        //----------------------------------------------
+        
+            // Abrimos el archivo de la Biblioteca de libros Biblioteca.txt
+            System.out.println("Abriendo archivo de la biblioteca...");
+
+           
+
+            System.out.println("Cerrando archivo de la biblioteca...");
+
+            System.out.println();
+            System.out.println("Archivos cerrados y procesamiento finalizado");
+            System.out.println("---------");
+            System.out.println();
+            System.out.println("Fin del programa.");
+    }
+}
